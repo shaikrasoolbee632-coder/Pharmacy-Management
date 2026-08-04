abc8396a458c864623b3e2ffd4bb3038e0521e1f
shaikrasoolbee632-coder <shaikrasoolbee632@gmail.com>
2026-08-04 13:21:34 +0530

    Updated backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "http://pharmacy-management-backend-1.onrender.com"
 });
 
 export default api;
